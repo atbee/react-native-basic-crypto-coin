4c7d2db030cc24d45cca32dd22b2afe4fb167183
--local <--local>
2020-02-27 18:28:31 +0700

    Edit style coin card

--- src/component/coinDetail.js
+++ src/component/coinDetail.js
@@ -7,12 +7,15 @@ const CoinDetail = (props) => {
     return (
         <View>
             <Card>
-                <View>
+                <View style={coinDetailStyle.containerMain}>
                     <Image style={coinDetailStyle.coinIconStyle}
                         source={{ uri: props.coin.coin_icon_url }} />
-                    <Text>{props.coin.symbol + ":" + props.coin.name}</Text>
+                    <View style={coinDetailStyle.containerCoinName}>
+                        <Text style={coinDetailStyle.textCoinSymbol}>{props.coin.symbol}</Text>
+                        <Text>{props.coin.name}</Text>
+                    </View>
+                    <Text style={coinDetailStyle.textCoinPrice}>${props.coin.price}</Text>
                 </View>
-
             </Card>
         </View>
     );
@@ -27,9 +30,27 @@ CoinDetail.defaultProps = {
 }
 
 const coinDetailStyle = {
+    containerMain: {
+        flexDirection: 'row'
+    },
+    containerCoinName: {
+        flexDirection: 'column',
+        marginLeft: 8,
+        flex: 1,
+    },
     coinIconStyle: {
         height: 50,
         width: 50
+    },
+    textCoinSymbol: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    textCoinName: {
+        fontSize: 14,
+    },
+    textCoinPrice: {
+        fontSize: 20,
     }
 };
 
