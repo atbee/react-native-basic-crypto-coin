aae7dbeb5f2406b088381232825141bd7794dcf6
Athibet Prawane <atb@odds.team>
2020-02-27 15:20:28 +0700

    Edit refactoring header

--- src/component/header.js
+++ src/component/header.js
@@ -2,25 +2,27 @@ import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
 // style
-const viewStyle = {
-    backgroundColor: '#F8F8F8',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 60,
-    paddingTop: 15,
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elavation: 2,
-    position: 'relative'
-};
-
-const textStyle = {
-    fontSize: 20
+const headerStyle = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elavation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 // init component
 const Header = () => {
+    const { viewStyle,textStyle } = headerStyle;
     return (
         <SafeAreaView>
             <View style={viewStyle}>
